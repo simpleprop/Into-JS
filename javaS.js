4c2dafc212b59791030f46bd29f8c64ba79d63f4
simpleprop <nikgurov06@gmail.com>
2021-01-06 15:59:14 +0200

    Initial commit

---
+++ javaS.js
@@ -0,0 +1,5 @@
+alert("Hello");
+var Location = prompt('WERE DO YOU L I V E?', "im scared");
+alert("You live in " + Location);
+console.log("You live in " + Location);
+document.write("You live in " + Location)
